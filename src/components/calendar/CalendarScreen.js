ffcdbfbbc6bb79dbe6ec61920e39cacc5512f3d3
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-02-01 08:53:17 +0100

    display calendar

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,27 @@
 import React from 'react'
-import { Navbar } from '../ui/Navbar'
-
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import { Navbar } from '../ui/Navbar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+const localizer = momentLocalizer(moment); 
+const events = [{
+  title: 'My Birdthay',
+  start: moment().toDate(),
+  end: moment().add(2, 'hours').toDate(),
+  bgcolor: '#fafafa'
+}]
 export const CalendarScreen = () => {
   return (
-    <div>
+    <div className="calendar-screen">
      <Navbar/>
+     <Calendar
+       localizer={localizer}
+       events={events}
+       startAccessor="start"
+       endAccessor="end"
+      />
+
+
     </div>
   )
 }
